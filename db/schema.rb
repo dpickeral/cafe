a19d0aab89b8db8e6dc87154ee04dc4d07eb1a78
David Pickeral <dwpickeral@gmail.com>
2015-10-26 20:32:32 -0400

    Pizza no crust

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026220208) do
+ActiveRecord::Schema.define(version: 20151026234403) do
 
   create_table "pizzas", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151026220208) do
     t.float    "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "photo"
   end
 
 end
